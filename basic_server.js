db12b5e04b5e7807ee5e571fdaea7ef3d93af5ab
Dave Mackey <dave@davemackey.net>
2020-11-17 12:34:21 -0500

    Add non-std library, use format function.

--- basic_server.js
+++ basic_server.js
@@ -3,6 +3,9 @@
 // Imports always done with absolute path
 import { serve } from 'https://deno.land/std/http/server.ts';
 
+// This is not part of the Deno std library
+import { format } from 'https://deno.land/x/date_fns/index.js';
+
 const url = 'http://hn.algolia.com/api/v1/search?query=javascript';
 
 const server = serve({ port: 8000 });
@@ -13,7 +16,10 @@ for await (const req of server) {
     const stories = result.hits.map((hit) => ({
         title: hit.title,
         url: hit.url,
-        createdAt: hit.created_at_i,
+        createdAt: format(
+            new Date(hit.created_at_i * 1000),
+            'yyy-MM-dd'
+        )
     }));
 
     req.respond({ body: JSON.stringify(stories) });
