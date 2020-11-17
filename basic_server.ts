899c598e0a290834adf96a1e8fe0d12177330f48
Dave Mackey <dave@davemackey.net>
2020-11-17 13:45:56 -0500

    Use env file to store port.

--- basic_server.ts
+++ basic_server.ts
@@ -1,14 +1,18 @@
-// To run: deno run --allow-net basic_server.js
+// To run: deno run --allow-net --allow-read basic_server.js
 
 // Imports always done with absolute path
 import { serve } from 'https://deno.land/std/http/server.ts';
+import { config } from 'https://deno.land/x/dotenv/mod.ts';
 
 // Can use relative paths for files within project
 import { mapStory } from './stories.ts'
 
 const url = 'http://hn.algolia.com/api/v1/search?query=javascript';
 
-const server = serve({ port: 8000 });
+const server = serve({
+    // We parse as int since it is stored in env as string
+    port: parseInt(config()['PORT']),
+});
 
 for await (const req of server) {
     const result = await fetch(url).then((result) => result.json());
