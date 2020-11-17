76e9be0d3e204e30526684985af2fb826f7d1ad6
Dave Mackey <dave@davemackey.net>
2020-11-17 12:38:14 -0500

    Import module from additional project file

--- basic_server.js
+++ basic_server.js
@@ -3,8 +3,8 @@
 // Imports always done with absolute path
 import { serve } from 'https://deno.land/std/http/server.ts';
 
-// This is not part of the Deno std library
-import { format } from 'https://deno.land/x/date_fns/index.js';
+// Can use relative paths for files within project
+import { mapStory } from './stories.js'
 
 const url = 'http://hn.algolia.com/api/v1/search?query=javascript';
 
@@ -13,14 +13,7 @@ const server = serve({ port: 8000 });
 for await (const req of server) {
     const result = await fetch(url).then((result) => result.json());
 
-    const stories = result.hits.map((hit) => ({
-        title: hit.title,
-        url: hit.url,
-        createdAt: format(
-            new Date(hit.created_at_i * 1000),
-            'yyy-MM-dd'
-        )
-    }));
-
+    const stories = result.hits.map(mapStory);
+    
     req.respond({ body: JSON.stringify(stories) });
 }
