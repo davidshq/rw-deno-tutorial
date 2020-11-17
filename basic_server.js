6f3221ed3aa65dda382fddc2e82a03660eb931c3
Dave Mackey <dave@davemackey.net>
2020-11-17 12:25:05 -0500

    Implement basic web server.

---
+++ basic_server.js
@@ -0,0 +1,10 @@
+// To run: deno run --allow-net basic_server.js
+
+// Imports always done with absolute path
+import { serve } from 'https://deno.land/std/http/server.ts';
+
+const server = serve({ port: 8000 });
+
+for await (const req of server) {
+    req.respond({ body: 'Hello Deno' });
+}
