9afa4589da4564d1421ef78d092107a605053112
Dave Mackey <dave@davemackey.net>
2020-11-18 10:19:40 -0500

    Use modular models as data sources

--- server.ts
+++ server.ts
@@ -6,64 +6,29 @@ import {
 
 import { v4 } from 'https://deno.land/std/uuid/mod.ts';
 
+import models from './models/index.ts';
+
 const port = 8000;
 const app = new Application();
-
-interface User {
-    id: string;
-    username: string;
-}
-
-const users = new Map<string, User>();
-
-users.set('1', {
-    id: '1',
-    username: 'Robin Wieruch',
-});
-
-users.set('2', {
-    id: '2',
-    username: 'Dave Davids',
-});
-
-interface Message {
-    id: string;
-    text: string;
-    userId: string;
-}
-
-const messages = new Map<string, Message>();
-
-messages.set('1', {
-    id: '1',
-    text: 'Hello World',
-    userId: '1',
-});
-
-messages.set('2', {
-    id: '2',
-    text: 'Bye World',
-    userId: '2',
-});
 
 const router = new Router();
 
 router.get('/users', (ctx) => {
-    ctx.response.body = Array.from(users.values());
+    ctx.response.body = Array.from(ctx.state.models.users.values());
 });
 
 router.get('/users/:userId', (ctx) => {
     const { userId } = helpers.getQuery(ctx, { mergeParams: true });
-    ctx.response.body = users.get(userId);
+    ctx.response.body = ctx.state.models.users.get(userId);
 });
 
 router.get('/messages', (ctx) => {
-    ctx.response.body = Array.from(messages.values());
+    ctx.response.body = Array.from(ctx.state.models.messages.values());
 });
 
 router.get('/messages/:messageId', (ctx) => {
     const { messageId } = helpers.getQuery(ctx, { mergeParams: true });
-    ctx.response.body = messages.get(messageId);
+    ctx.response.body = ctx.state.models.messages.get(messageId);
 });
 
 router.post('/messages', async (ctx) => {
@@ -72,29 +37,32 @@ router.post('/messages', async (ctx) => {
     const { value } = ctx.request.body({ type: 'json' });
     const { text } = await value;
 
-    messages.set(id, {
+    ctx.state.models.messages.set(id, {
         id,
         text,
         userId: ctx.state.me.id,
     });
 
-    ctx.response.body = messages.get(id);
+    ctx.response.body = ctx.state.models.messages.get(id);
 });
 
 router.delete('/messages/:messageId', async(ctx) => {
     const { messageId } = helpers.getQuery(ctx, { mergeParams: true });
 
-    const isDeleted = messages.delete(messageId);
+    const isDeleted = ctx.state.models.messages.delete(messageId);
 
     ctx.response.body = isDeleted;
 });
 
 router.get('/session', (ctx) => {
-    ctx.response.body = users.get(ctx.state.me.id);
+    ctx.response.body = ctx.state.models.users.get(ctx.state.me.id);
 });
 
 app.use(async (ctx, next) => {
-    ctx.state = {me: users.get('1')};
+    ctx.state = {
+        models,
+        me: models.users.get('1'),
+    };
 
     await next();
 });
